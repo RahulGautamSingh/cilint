76d6d1790288c6a38a7a8ec77634f3b93b6101c1
RahulGautamSingh <rahultesnik@gmail.com>
2022-01-11 20:06:21 +0545

    lint

--- index.js
+++ index.js
@@ -1,48 +1,5 @@
-const { valid, clean, explain } = require("./lib/version");
+function sum(a, b) {
+  return a + b;
+}
+module.exports = {sum}
 
-const { lt, le, eq, ne, ge, gt, compare, rcompare } = require("./lib/operator");
-
-const {
-  filter,
-  maxSatisfying,
-  minSatisfying,
-  RANGE_PATTERN,
-  satisfies,
-  validRange,
-} = require("./lib/specifier");
-
-const { major, minor, patch, inc } = require("./lib/semantic");
-
-module.exports = {
-  // version
-  valid,
-  clean,
-  explain,
-
-  // operator
-  lt,
-  le,
-  lte: le,
-  eq,
-  ne,
-  neq: ne,
-  ge,
-  gte: ge,
-  gt,
-  compare,
-  rcompare,
-
-  // range
-  filter,
-  maxSatisfying,
-  minSatisfying,
-  RANGE_PATTERN,
-  satisfies,
-  validRange,
-
-  // semantic
-  major,
-  minor,
-  patch,
-  inc,
-};
